c4ab713bda0635bb4c47c7bfbcc2736368712ec2
josephadogeriopao <joseph.adogeri@orleansassessors.com>
2025-08-27 12:36:19 -0500

    first commit

---
+++ postgresql-demo/postgresql-demo/src/main/java/com/example/postgresql_demo/PostgresqlDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.postgresql_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostgresqlDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostgresqlDemoApplication.class, args);
+	}
+
+}
---
+++ postgresql-demo/postgresql-demo/src/test/java/com/example/postgresql_demo/PostgresqlDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.postgresql_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostgresqlDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
